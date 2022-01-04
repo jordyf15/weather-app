5721084ddfd25173e705e6e08e82496257cf6c13
jordyf15 <jordyferdian88@gmail.com>
2022-01-04 09:55:18 +0700

    created project start

---
+++ src/index.js
@@ -0,0 +1,15 @@
+async function getWeatherData(location){
+    const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f4d3afe8690a5aa5fefaddb6259abf79`;
+    const response = await fetch(apiCall);
+    const weatherData = await response.json();
+    return weatherData;
+}
+
+const weatherForm = document.querySelector('#weather-form');
+const locationInput = document.querySelector('#location-input');
+weatherForm.addEventListener('submit',async (e)=>{
+    e.preventDefault();
+    const location = locationInput.value;
+    const weatherData = await getWeatherData(location);
+    console.log(weatherData);
+});
